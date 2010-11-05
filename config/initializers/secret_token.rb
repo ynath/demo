d1cc51c953bb307e6bed3031a18ae37210c75217
System Administrator <root@Yogis-MacBook-Pro.local>
2010-11-05 11:38:10 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Demo::Application.config.secret_token = '9530656bc5485cfc79389a41e84c5fd5f6cdc1bde20e27642feb238ad075cdbdc5a9d90902763c85c0e452e7ae666336e31e3e9cc57bd969ded01296bcd2fa9b'
